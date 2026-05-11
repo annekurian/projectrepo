d502c73a4ea89951c1500285be9e0d945ed310b0
annekurian <anne.kurian14@gmail.com>
2026-05-11 18:43:21 +0800

    Adding task-1.js for Module-5 Tasks

---
+++ module-5/task-1.js
@@ -0,0 +1,11 @@
+let name = "Anne Kurian";
+let age = 38;
+let isStudent = false;
+console.log("Name: ", name, " | Type: ", typeof name);
+console.log("Age: ", age, " | Type: ", typeof age);
+console.log(
+  `Is ${name} a student: `,
+  isStudent ? "Yes" : "No",
+  " | Type: ",
+  typeof isStudent,
+);
