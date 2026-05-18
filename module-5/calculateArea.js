292076fd8f6820e68a8f3b7d6326dc65200b4353
annekurian <anne.kurian14@gmail.com>
2026-05-18 12:03:27 +0800

    module-5 tasks

---
+++ module-5/calculateArea.js
@@ -0,0 +1,6 @@
+function calculateArea(length, width) {
+  return length * width;
+}
+
+const area = calculateArea(5, 2.5);
+console.log("Area of the rectangle: ", area);
---
+++ module-5/task-3-loops-arrays.js
@@ -0,0 +1,5 @@
+const fruits = ["apple", "banana", "mango", "orange"];
+
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
