26b04348f879ed94e32c0a5e1361b2c69f1ff3a4
annekurian <anne.kurian14@gmail.com>
2026-07-20 13:00:16 +0530

    Test data for students database

---
+++ module-12/express-restapi-lab/test-data.sql
@@ -0,0 +1,12 @@
+/* Creating 10 records in students table */
+
+INSERT INTO students VALUES (101, "Sarah", "sarah@example.com", "Science"),
+(102, "Vivek", "vivek@example.com", "Commerce"),
+(103, "Anne", "anne@example.com", "Science"),
+(104, "Jane", "jane@example.com", "Commerce"),
+(105, "Matt", "matt@example.com", "Commerce"),
+(106, "Arun", "arun@example.com", "Science"),
+(107, "Alice", "alice@example.com", "Commerce"),
+(108, "Neil", "neil@example.com", "Science"),
+(109, "Alex", "alex@example.com", "Science"),
+(110, "John", "john@example.com", "Science"),
